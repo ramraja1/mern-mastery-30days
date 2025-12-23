c368498612b49432dbfa7e5957717626feb59c90
Pawan Tiwari <ramtiwari7081@gmail.com>
2025-12-24 01:04:43 +0530

    added 17th task in level-3

--- Level-3/controler/notesControler.js
+++ Level-3/controler/notesControler.js
@@ -1,13 +1,21 @@
 import notes from "../model/note.js";
 export const viewNotes = async (req, res) => {
-  const data = await notes.findById();
+   try{
+        const data = await notes.find({userId:req.user.id});
+        res.send(data);
+   }
+   catch(error){
+        return res.status(500).json({message:"Something went wrong..",error:error});
+   }
+  
+
 };
 
 export const createNotes = async (req, res) => {
   try {
     const { title, content } = req.body;
     if (!title || !content)
-      return res.status(404).json({ message: "All fields are required..." });
+      return res.status(400).json({ message: "All fields are required..." });
 
     const data = await notes.create({
       title,
@@ -15,11 +23,20 @@ export const createNotes = async (req, res) => {
       userId: req.user.id,
     });
 
-    res.status(200).json({ message: "Notes created succesfully..." });
+    res.status(201).json({ message: "Notes created succesfully...",data:data });
   } catch (error) {
     return res.status(500).json({
       message: "Something went wrong ..",
       error: error,
     });
   }
+};
+
+export const updateNotes=async (req,res)=>{
+    const {title,content,id}=req.body;
+
+    const check=await notes.findById(id);
+    if(!check)return res.status(400).json({message:"this data is not available"});
+    const data =await notes.findByIdAndUpdate(id,{title,content});
+    res.send(data);
 };
--- Level-3/route/notesRoutes.js
+++ Level-3/route/notesRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createNotes,viewNotes } from "../controler/notesControler.js";
+import { createNotes,viewNotes,updateNotes } from "../controler/notesControler.js";
 
 const router=Router();
 
 router.post("/create",createNotes);
-router.get("/view",viewNotes)
+router.get("/view",viewNotes);
+router.put("/update",updateNotes);
 
 export default router;
